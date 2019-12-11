6dd61c838f7341a6adefbe7d04471875d61ab220
Nick <ribuluo189@gmail.com>
2019-12-11 17:44:32 +0800

    //停止网络 //启动网络带CA couchdb

--- egg-example/app/controller/init.js
+++ egg-example/app/controller/init.js
@@ -4,8 +4,9 @@ const Controller = require('egg').Controller;
 const Client = require('fabric-client');
 const utils = require('fabric-client/lib/utils.js');
 const logger = utils.getLogger('INIT');
-var path = require('path');
-
+const path = require('path');
+const dirPath = path.join(__dirname, '../fabric/first-network/');
+const filePath = path.join(__dirname, '../fabric/first-network/byfn.sh');
 const { exec, execFileSync, execFile } = require('child_process');
 
 class InitController extends Controller {
@@ -21,42 +22,47 @@ class InitController extends Controller {
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
     })
-
-    const filePath = path.join(__dirname,'../fabric/first-network/byfn.sh');
-
-    exec(filePath, (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    })
-
     ctx.body = 'hi, init';
   }
 
   async init_generate() {
     const { ctx } = this;
     logger.info('hi, init_generate');
-    const dirPath = path.join(__dirname, '../fabric/first-network/');
-    const filePath = path.join(__dirname, '../fabric/first-network/byfn.sh');
     const stdout = execFileSync(filePath, ['generate'], { cwd: dirPath });
-    logger.info(filePath+ 'generate result stdout:\n',stdout);
+    logger.info(filePath+ ' generate result stdout:\n',stdout);
 
     ctx.body = 'hi, init_generate';
+  }
+
+
+  async init_network_stop() {
+    const { ctx } = this;
+    logger.info('hi, init_network_stop');
+    const stdout = execFileSync(filePath, ['down'], { cwd: dirPath });
+    logger.info(filePath+ ' up result stdout:\n',stdout);
+
+    ctx.body = 'hi, init_network_stop';
   }
 
   async init_network_start() {
     const { ctx } = this;
     logger.info('hi, init_network_start');
-    const dirPath = path.join(__dirname, '../fabric/first-network/');
-    const filePath = path.join(__dirname, '../fabric/first-network/byfn.sh');
     const stdout = execFileSync(filePath, ['up'], { cwd: dirPath });
     logger.info(filePath+ ' up result stdout:\n',stdout);
 
     ctx.body = 'hi, init_network_start';
   }
+
+
+  async init_network_start_a_couchdb() {
+    const { ctx } = this;
+    logger.info('hi, init_network_start_a_couchdb');
+    const stdout = execFileSync(filePath, ['up', '-a', '-n', '-s', 'couchdb'], { cwd: dirPath });
+    logger.info(filePath+ ' up result stdout:\n',stdout);
+
+    ctx.body = 'hi, init_network_start_a_couchdb';
+  }
+
 
 }
 
